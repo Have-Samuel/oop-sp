93bff7a93f10071b5758ffe8a2647db659cef51a
Have Samuel <samiehave9@gmail.com>
2023-04-25 16:58:28 +0300

    Add Linters

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Adding functions as Methods
-const add = (x,y) => (x + y);
-const mult = (x,y) => (x * y);
+const add = (x, y) => (x + y);
+const mult = (x, y) => (x * y);
 const sqr = (x) => (x * x);
-const pwr = (x, y) => (x **y)
+const pwr = (x, y) => (x ** y);
 
 const myMath = {};
 
@@ -11,10 +11,10 @@ myMath.mult = mult;
 myMath.pwr = pwr;
 myMath.sqr = sqr;
 // One way
-myMath.add(3, 4)
-7
-myMath.mult(5,5)
-25
-myMath.sqr(5)
-25
-myMath.pwr(2,3)
+myMath.add(3, 4);
+// 7;
+myMath.mult(5, 5);
+// 25;
+myMath.sqr(5);
+// 25;
+myMath.pwr(2, 3);
