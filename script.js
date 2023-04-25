9d655a15b2337887211638788b4d07bcd2285e6b
Have Samuel <samiehave9@gmail.com>
2023-04-25 17:06:49 +0300

    Using 2 of adding functions

--- script.js
+++ script.js
@@ -4,17 +4,24 @@ const mult = (x, y) => (x * y);
 const sqr = (x) => (x * x);
 const pwr = (x, y) => (x ** y);
 
-const myMath = {};
+// const myMath = {};
+// MTD III
+// myMath.add = add;
+// myMath.mult = mult;
+// myMath.pwr = pwr;
+// myMath.sqr = sqr;
 
-myMath.add = add;
-myMath.mult = mult;
-myMath.pwr = pwr;
-myMath.sqr = sqr;
-// One way
-myMath.add(3, 4);
-// 7;
-myMath.mult(5, 5);
-// 25;
-myMath.sqr(5);
-// 25;
-myMath.pwr(2, 3);
+// myMath.add(3, 4);
+// // 7;
+// myMath.mult(5, 5);
+// // 25;
+// myMath.sqr(5);
+// // 25;
+// myMath.pwr(2, 3);
+
+// // MTD II
+// const myMath = {
+//   add, mult, sqr, pwr,
+// };
+// myMath.pwr(3, 3);
+// // 27
