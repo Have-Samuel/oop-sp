37560cb56327853f133608ab69b6010ad91e612e
Have Samuel <samiehave9@gmail.com>
2023-04-26 16:12:16 +0300

    Each vehicle instance should have access to a method called honk, which returns the string “Beep.” Each vehicle instance should have a method called toString, which returns the string containing the make, model and year.

--- script.js
+++ script.js
@@ -302,3 +302,45 @@ class Motorcycle extends Vehicle {
     return 'VROOM!!!';
   }
 }
+
+// PART 4
+// Create a class for a Garage.
+// It should have a property called vehicles which will store an array of vehicles,
+// and a property called capacity which is a number indicating how many vehicles
+// will fit in the garage. When you create a garage, vehicles will always be empty;
+// you only need to provide the capacity.
+
+// A garage should also have an add method,
+// which attempts to add a vehicle to the array of vehicles.
+// However, if you try to add something which is not a vehicle,
+// the garage should return the message “Only vehicles are allowed in here!”.
+// Also, if the garage is at capacity, it should say “Sorry, we’re full.”
+
+class Garage {
+  constructor(capacity) {
+    this.vehicles = [];
+    this.capacity = capacity;
+  }
+
+  add(newVehicle) {
+    if (!(newVehicle instanceof Vehicle)) {
+      return 'Only vehicles are allowed in here!';
+    }
+    if (this.vehicles.length >= this.capacity) {
+      return 'Sorry, we’re full.';
+    }
+    this.vehicles.push(newVehicle);
+    return 'Vehicle added!';
+  }
+}
+
+const garage = new Garage(2);
+garage.vehicles; // []
+garage.add(new Car('Hyundai', 'Elantra', 2015)); // "Vehicle added!"
+garage.vehicles; // [Car]
+garage.add('Taco'); // "Only vehicles are allowed in here!"
+garage.add(new Motorcycle('Honda', 'Nighthawk', 2000));
+// "Vehicle added!"
+garage.vehicles; // [Car, Motorcycle]
+garage.add(new Motorcycle('Honda', 'Nighthawk', 2001));
+// "Sorry, we're full."
